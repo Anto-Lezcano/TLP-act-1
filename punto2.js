5bb87fd19f94090c432a533808a476fa13f5b44b
Anto-Lezcano <lezcanoantonella85@gmail.com>
2025-08-07 12:19:18 -0300

    se completo el punto 2

---
+++ punto2.js
@@ -0,0 +1,48 @@
+class CuentaBancaria {
+  titular;
+  #saldo;
+  constructor(titular, saldoInicial) {
+    this.titular = titular;
+    this.#saldo = saldoInicial >= 0 ? saldoInicial : 0;
+  }
+
+  get saldo() {
+    return this.#saldo;
+  }
+
+  set saldo(nuevoSaldo) {
+    if (nuevoSaldo >= 0) {
+      this.#saldo = nuevoSaldo;
+    } else {
+      console.log("El saldo no puede ser negativo.");
+    }
+  }
+
+  depositar(monto) {
+    if (monto > 0) {
+      this.#saldo += monto;
+      console.log(`Depósito exitoso. Nuevo saldo: $${this.#saldo}`);
+    } else {
+      console.log("El monto debe ser mayor a cero.");
+    }
+  }
+
+  extraer(monto) {
+    if (monto > this.#saldo) {
+      console.log("Fondos insuficientes.");
+    } else {
+      this.#saldo -= monto;
+      console.log(`Extracción exitosa. Nuevo saldo: $${this.#saldo}`);
+    }
+  }
+  consultarSaldo() {
+    console.log(`Saldo actual: $${this.#saldo}`);
+  }
+}
+const cuenta = new CuentaBancaria("Susana", 1000);
+cuenta.consultarSaldo();
+cuenta.depositar(200);
+cuenta.extraer(1300);
+cuenta.saldo = -500;
+console.log(cuenta.saldo);
+cuenta.extraer(200);
